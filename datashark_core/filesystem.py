361e0fc0c25bd1eb29466d154afb00eb582d3ce5
koromodako <koromodako@gmail.com>
2021-09-11 18:42:34 +0200

    Improve logging when handling path traversal issue

--- datashark_core/filesystem.py
+++ datashark_core/filesystem.py
@@ -1,7 +1,7 @@
 """Filesystem-related helpers
 """
 from pathlib import Path
-
+from . import LOGGER
 
 def ensure_parent_dir(filepath: Path):
     """Ensure parent directory hierarchy exists for given filepath"""
@@ -12,5 +12,7 @@ def prepend_workdir(workdir: Path, relative_path: Path):
     """Prepend workdir and prevent path traversal"""
     filepath = (workdir / relative_path).resolve()
     if not filepath.is_relative_to(workdir):
+        LOGGER.warning("workdir: %s", workdir)
+        LOGGER.warning("filepath: %s", filepath)
         raise ValueError("path traversal attempt prevented!")
     return filepath
